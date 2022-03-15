aae20af226b72650d990478bdd3a360a6ae8c7fc
Rodrigo Perote <73118133+perotedev@users.noreply.github.com>
2022-03-15 10:55:48 -0400

    Update main.py
    Acrescentado mais respostas

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ def _get_answer(question: str) -> str:
         " ":" Me pergunte algo",
         "ola":"Ola, tudo bem?",
         "tudo bem?":"Sim, esta tudo bem",
-        "sim, e com voce?":"Tudo bem."
+        "sim, e com voce?":"Tudo bem.",
+        "sis, estou bem":"Que bom!",
+        "bom dia":"Oi, bom dia para você também!",
+        "boa tarde":"Oi, boa tarde para você também!",
+        "boa noite":"Oi, boa noite para você também!",
+        "estou triste":"Sinto muito, o que houve?",
+        "estou feliz":"Legal, isso e muito bom",
+        "como voce esta?":"Estou bem, e voce?",
+        "voce gosta musica?":"Sim, minha cantora favoria e Camila Cabello"
         
     }.get(question.lower())
 
@@ -26,7 +34,7 @@ def ask(msg:Mensagem):
         }
     else:
         return HTTPException(
-            detail="Eu nao sei ainda o que voce esta falando, por favor me pergunte algo que eu sei",
+            detail="Ainda nao sei o que voce esta falando, por favor me pergunte algo que eu sei",
             status_code= status.HTTP_400_BAD_REQUEST
         )
 
